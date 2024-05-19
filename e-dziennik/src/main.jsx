87025230776dc65817734426b3841208991b68bc
Olkania <ola.gadzinska@gmail.com>
2024-05-19 13:19:18 +0200

    some changes w/ installation

--- e-dziennik/src/main.jsx
+++ e-dziennik/src/main.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Login from './Login.jsx'
+import Dashboard from './Dashboard.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    
     <Login />
+    <Dashboard />
   </React.StrictMode>,
 )
