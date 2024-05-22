47dcdcc49d3de40157947fa7275da88a2bf81a7d
Olkania <ola.gadzinska@gmail.com>
2024-05-22 18:54:36 +0200

    Visualisation ggrades, plan

--- e-dziennik/src/Grades.jsx
+++ e-dziennik/src/Grades.jsx
@@ -3,15 +3,51 @@ import './App.css'
 import Header from './Layouts/Header'
 import Home from './Layouts/Home'
 import Sidebar_teacher from './Layouts/Sidebar_teacher'
+import Select from 'react-select'
+
+
 
 function Grades() {
 
+  // TODO      ----     Data from data base ---- classes which teacher has
+  const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+  ]
+
+// TABLE ---- TODO ---- jakos ogarnac ilosc wierszy i kolumn tyle ile jest uczniow/ ocen itd
+// DODAJ OCENE ---- FORMULARZ DODAWANIA OCENY/ EDYTOWANIA
   return (
     <div className='grid-container'>
     <Header />
     <Sidebar_teacher />
     <main className='main-container'>
-      <img src="https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGhvdG98ZW58MHx8MHx8fDA%3D" />
+      <div className='main-header'>
+        <Select className='dropdown' options={options} />
+        <button className='button-main'>
+          Dodaj ocenę
+        </button>
+      </div>
+      <table className='table-main'>
+      <thead>
+      <tr>
+        <th>Hours</th>
+        <th>Events</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>8:00 - 8:45</td>
+        <td>Event 1
+        </td>
+      </tr>
+      <tr>
+      <td>8:55 - 9:40</td>
+        <td>Event 2</td>
+      </tr>
+    </tbody>
+      </table>
     </main>
     </div>
   )
--- e-dziennik/src/Plan.jsx
+++ e-dziennik/src/Plan.jsx
@@ -3,15 +3,53 @@ import './App.css'
 import Header from './Layouts/Header'
 import Home from './Layouts/Home'
 import Sidebar_teacher from './Layouts/Sidebar_teacher'
+import {BsCaretLeftFill, BsCaretRightFill} from 'react-icons/bs'
 
 function Plan() {
-
+// TABLE ---- TODO ---- jakos ogarnac ilosc wierszy i kolumn tyle ile jest lekcji (z 8/9) i dni tygodnia (daty ?)
   return (
     <div className='grid-container'>
     <Header />
     <Sidebar_teacher />
     <main className='main-container'>
-      <img src="https://images.pexels.com/photos/3680219/pexels-photo-3680219.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+    <div className='header-main-right'>
+        <button className='arrow-button'>
+        <BsCaretLeftFill className='arrow-icon' />
+      </button>
+      <button className='arrow-button'>
+        <BsCaretRightFill className='arrow-icon' />
+      </button>
+      </div>
+      <table className='table-main'>
+      <thead>
+      <tr>
+        <th>Hours</th>
+        <th>Poniedziałek</th>
+        <th>Wtorek</th>
+        <th>Środa</th>
+        <th>Czwartek</th>
+        <th>Piątek</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>8:00 - 8:45</td>
+        <td>Event 1</td>
+        <td>Event 1</td>
+        <td>Event 1</td>
+        <td>Event 1</td>
+        <td>Event 1</td>
+      </tr>
+      <tr>
+      <td>8:55 - 9:40</td>
+        <td>Event 2</td>
+        <td>Event 2</td>
+        <td>Event 2</td>
+        <td>Event 2</td>
+        <td>Event 2</td>
+      </tr>
+    </tbody>
+      </table>
     </main>
     </div>
   )
