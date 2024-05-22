095896be44d876da33095bedb007a2cb0e0fce87
Olkania <ola.gadzinska@gmail.com>
2024-05-22 19:12:17 +0200

    Visualization pof obecnosc

--- e-dziennik/src/Presence.jsx
+++ e-dziennik/src/Presence.jsx
@@ -1,17 +1,51 @@
 import { useState, } from 'react'
 import './App.css'
 import Header from './Layouts/Header'
-import Home from './Layouts/Home'
 import Sidebar_teacher from './Layouts/Sidebar_teacher'
+import Select from 'react-select'
+
 
 function Presence() {
 
+  // TODO      ----     Data from data base ---- classes which teacher has
+  const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+  ]
+// TABLE ---- TODO ---- jakos ogarnac ilosc wierszy i kolumn tyle ile jest uczniow/ dni itd
   return (
     <div className='grid-container'>
     <Header />
     <Sidebar_teacher />
     <main className='main-container'>
-      <img src="https://static.vecteezy.com/vite/assets/photo-masthead-375-b8ae1548.webp" />
+      <div className='main-header'>
+        <Select className='dropdown' options={options} />
+        <button className='button-main'>
+          Dodaj obecność
+        </button>
+      </div>
+      <table className='table-main'>
+      <thead>
+      <tr>
+        <th>Uczeń</th>
+        <th>Wypisane</th>
+        <th>Dni</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>8:00 - 8:45</td>
+        <td>Event 1</td>
+        <td>Event 1</td>
+      </tr>
+      <tr>
+      <td>8:55 - 9:40</td>
+        <td>Event 2</td>
+        <td>X</td>
+      </tr>
+    </tbody>
+      </table>
     </main>
     </div>
   )
