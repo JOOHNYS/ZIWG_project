793ca5920eeace081db43abfd220a7289f87267e
Maja <80975661+Maja06063@users.noreply.github.com>
2024-06-12 19:16:12 +0200

    Front cookies oceny (#2)
    * Ogarniecie generowania cookies przy logowaniu
    
    * Rozdzielenie zakladki oceny dla nauczyciela i ucznia
    
    * Wyliczanie nieobecnych

---
+++ e-dziennik/src/Layouts/Grades_student.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Cookies from 'js-cookie';
+
+function Grades_student() {
+
+    return (
+        <main className='main-container'>
+            <div className='main-header-tables'>
+      </div>
+      <table className='table-main'>
+      <thead>
+      <tr>
+        <th>Hours</th>
+        <th>Events</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>8:00 - 8:45</td>
+        <td>Event 1
+        </td>
+      </tr>
+      <tr>
+      <td>8:55 - 9:40</td>
+        <td>Event 2</td>
+      </tr>
+    </tbody>
+      </table>
+    </main>
+    )
+}
+
+export default Grades_student;
